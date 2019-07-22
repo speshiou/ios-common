3a1cd2f28ba81bcafe471a59f6cbc907fd42a7c0
pshaung <speshiou@gmail.com>
2019-07-22 12:31:11 +0800

    Initial commit

---
+++ iOSCommon/iOSCommon.h
@@ -0,0 +1,19 @@
+//
+//  iOSCommon.h
+//  iOSCommon
+//
+//  Created by Pei-shiou Huang on 7/22/19.
+//  Copyright © 2019 YUHO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSCommon.
+FOUNDATION_EXPORT double iOSCommonVersionNumber;
+
+//! Project version string for iOSCommon.
+FOUNDATION_EXPORT const unsigned char iOSCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSCommon/PublicHeader.h>
+
+
